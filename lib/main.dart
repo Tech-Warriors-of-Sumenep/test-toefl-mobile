d21179a7cb29aba6e253ed6bbb57f807ec92cfe2
ekosarr <ekoafi123@gmail.com>
2024-05-06 10:28:59 +0700

    color

--- lib/main.dart
+++ lib/main.dart
@@ -14,9 +14,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.deepPurple),
-      ),
       home: const HomeLearningReadingPages(),
     );
   }
